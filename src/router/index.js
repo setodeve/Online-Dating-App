62e7399ca6748e50b72ad74c1a0737362a0ee038
setodeve <keinick11@outlook.com>
2022-05-02 21:48:37 +0900

    Add User Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import ChatView from '../pages/chat.vue'
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/user/id/chat',
+    name: 'chat',
+    component: ChatView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/message-list',
+    name: 'message-list',
+    component: () => import('../pages/message-list.vue')
+  },
+  {
+    path: '/user/id',
+    name: 'user-detail',
+    component: () => import('../pages/user-detail.vue')
+  },
+  {
+    path: '/',
+    name: 'user-list',
+    component: () => import('../pages/user-list.vue')
   }
 ]
 
