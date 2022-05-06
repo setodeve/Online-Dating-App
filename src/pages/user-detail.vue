bb601c37b3cf95be5c33378893192ef3ffab99e6
setodeve <keinick11@outlook.com>
2022-05-07 03:36:59 +0900

    User List

--- src/pages/user-detail.vue
+++ src/pages/user-detail.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="user-detail container">
-    <h1>This is an about page</h1>
     <div>{{ user["first"] }} </div>
   </div>
   <div class="d-flex flex-wrap">
--- src/pages/user-list.vue
+++ src/pages/user-list.vue
@@ -3,10 +3,12 @@
     <router-link :to="{ path: '/message-list'}">Move to MessageList</router-link>
     <ul class="d-flex flex-wrap" style="list-style-type: none;">
       <li v-for="user in users" :key="user.id" class="col-3 card m-2" style="width: 10rem;">
-          <img class="card-img-top" :src='user.value.picture.thumbnail' alt="Card image cap">
+        <img class="card-img-top" :src='user.value.picture.thumbnail' alt="Card image cap">
         <div class="card-body d-flex flex-wrap">
-          <router-link :to="{ path: '/user', query: { id: user.id }}" class="text-muted col-12">{{ user.value.name.first }}</router-link>
+          <div class="text-muted col-12">{{ user.value.name.first }}</div>
           <div class="text-muted col-12">age: {{ user.value.dob.age }}</div>
+          <router-link :to="{ path: '/user', query: { id: user.id }}" class="text-muted col-5 m-1">Details</router-link>
+          <router-link :to="`/user/${user.value.login.uuid}/chat`" class="text-muted col-5 m-1">Chat</router-link>
         </div>
       </li>
     </ul>
