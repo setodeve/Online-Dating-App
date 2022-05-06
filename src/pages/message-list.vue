030d58e9d3047d8eb402bc6e5d0985f48641e4bb
setodeve <keinick11@outlook.com>
2022-05-06 22:51:03 +0900

    Messages List

--- src/pages/message-list.vue
+++ src/pages/message-list.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="message-list">
-    <h1>This is an about page</h1>
+  <div class="message-list container">
+    <div>Users List You have had Message</div>
+    <ul class="d-flex flex-wrap" style="list-style-type: none;">
+      <li v-for="list in lists" :key="list.id" class="col-12">
+        <router-link :to="{ path: '/user', query: { id: list.id }}">{{ list.name.first }}</router-link>
+      </li>
+    </ul>
   </div>
-  
+
 </template>
+
+<script>
+export default {
+  name: 'MessageListView',
+  data() {
+    return {
+      lists : [],
+      /*
+        ["id":username],["id":username]
+      */
+    }
+  },
+  mounted(){
+    let messages = JSON.parse(localStorage.getItem("messages")) ;
+    for(let key in messages){
+      this.lists.push({id: key,name:this.getuserName(key)}) ;
+    }
+    console.log(this.lists) ;
+  },
+  methods:{
+    getuserName: function(id){
+      let tmp = this.$store.getters['users/getUserById'](id) ;
+      let user = tmp[0].value.name ;
+      return user ;
+    },
+  }
+}
+</script>
--- src/pages/user-list.vue
+++ src/pages/user-list.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="user-list container">
+    <router-link :to="{ path: '/message-list'}">Move to MessageList</router-link>
     <ul class="d-flex flex-wrap" style="list-style-type: none;">
       <li v-for="user in users" :key="user.id" class="col-3">
         <router-link :to="{ path: '/user', query: { id: user.id }}">{{ user.value.name.first }}</router-link>
