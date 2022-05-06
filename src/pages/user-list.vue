8c48f3e8ab7d3a29d83c6f32ccc477c76d7aa581
setodeve <keinick11@outlook.com>
2022-05-07 03:13:45 +0900

    User List

--- src/pages/user-list.vue
+++ src/pages/user-list.vue
@@ -2,8 +2,12 @@
   <div class="user-list container">
     <router-link :to="{ path: '/message-list'}">Move to MessageList</router-link>
     <ul class="d-flex flex-wrap" style="list-style-type: none;">
-      <li v-for="user in users" :key="user.id" class="col-3">
-        <router-link :to="{ path: '/user', query: { id: user.id }}">{{ user.value.name.first }}</router-link>
+      <li v-for="user in users" :key="user.id" class="col-3 card m-2" style="width: 10rem;">
+          <img class="card-img-top" :src='user.value.picture.thumbnail' alt="Card image cap">
+        <div class="card-body d-flex flex-wrap">
+          <router-link :to="{ path: '/user', query: { id: user.id }}" class="text-muted col-12">{{ user.value.name.first }}</router-link>
+          <div class="text-muted col-12">age: {{ user.value.dob.age }}</div>
+        </div>
       </li>
     </ul>
   </div>
