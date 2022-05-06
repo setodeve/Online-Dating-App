57216142f993f7cdfc54c9b7275c5f216b3cf190
setodeve <keinick11@outlook.com>
2022-05-07 04:11:50 +0900

    User Details

--- src/pages/user-detail.vue
+++ src/pages/user-detail.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="user-detail container">
-    <div>{{ user["first"] }} </div>
-  </div>
-  <div class="d-flex flex-wrap">
-    <router-link :to="`/user/${$route.query.id}/chat`" class="col-12">Start Chat With {{ user["first"] }}</router-link>
-    <router-link :to="`/`" class="col-12">Back to User List</router-link>
-  </div>
+    <div class="d-flex flex-wrap">
+      <router-link :to="`/`" class="col-12">Back to User List</router-link>
+    </div>
+    <div class="d-flex justify-content-center ">
+      <div class="col-12 card" style="width: 20rem;">
+        <img class="card-img-top" :src='image' alt="Card image cap">
+        <div class="card-body d-flex flex-wrap">
+          <div class="text-muted col-12">{{ name }}</div>
+          <div class="text-muted col-12">age: {{ age }}</div>
+          <div class="text-muted col-12">gender: {{ gender }}</div>
+          <div class="text-muted col-12">state: {{ state }}</div>
+          <router-link :to="`/user/${$route.query.id}/chat`" class="text-muted col-12 m-1">Chat</router-link>
+        </div>
+      </div>
+    </div>
 
+  </div>
 </template>
 
 
@@ -15,12 +25,20 @@ export default {
   name: 'UserDetailView',
   data() {
     return {
-      user : {},
+      name : "",
+      gender:"",
+      image:"",
+      age:0,
+      state:""
     }
   },
   mounted(){
     let tmp = this.$store.getters['users/getUserById'](this.$route.query.id) ;
-    this.user = tmp[0].value.name ;
+    this.name = tmp[0].value.name.first ;
+    this.gender = tmp[0].value.gender ;
+    this.image = tmp[0].value.picture.medium ;
+    this.age = tmp[0].value.dob.age ;
+    this.state = tmp[0].value.location.state ;
   },
 }
 </script>
